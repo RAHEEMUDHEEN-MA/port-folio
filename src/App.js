46eab0683a426d10845692cb5c7f2e2979d03aa8
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-04-25 21:30:52 +0530

    initial loading delay added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
+import { useState } from "react";
 import "./App.css";
 import HeroSection from "./Pages/HeroSection";
 
 function App() {
+  const [loading, setloading] = useState(true)
+  setTimeout(() => {
+    setloading(false)
+  }, 1000);
   return (
     <div>
-
-   <HeroSection/>
+     {!loading&&<HeroSection/>}
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,7 +14,8 @@ const NavBar = () => {
       className=" navbar-main  text-gray-300 text-1xl font-semibold w-full h-20 px-2 sm:px-24 flex justify-end sm:justify-between  items-center "
       style={{ boxShadow: "2px 2px 10px rgb(10, 10, 1" }}
     >
-      <div className="h-full  font-serif text-3xl text-pink-900 italic tracking-wide leading-tight">
+      <div className=" raheemTxt h-full  font-serif text-3xl text-pink-900 italic tracking-wide leading-tight">
+        {/* raheem */}
       </div>{" "}
       <div className="hidden sm:flex">
         <ul className="text-gray-500 hidden sm:flex items-center  gap-4  ">
@@ -22,7 +23,7 @@ const NavBar = () => {
           <li  className=" text-white' : 'text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 hover:scale-105">About</li>
           <li  className=" text-white' : 'text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 hover:scale-105">Projects</li>
           <li className=" text-white' : 'text-gray-300 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 hover:scale-105">Contact</li>
-          <button className="bg-gradient-to-br from-purple-600 to-black rounded-md px-5 py-2 text-gray-300 hover:text-white hover:bg-purple-700">
+          <button  className="bg-gradient-to-br from-purple-600 to-gray-950 rounded-md px-5 py-2 text-gray-300 hover:text-white hover:bg-purple-700 animate-none">
             Let's Talk
           </button>
         </ul>
