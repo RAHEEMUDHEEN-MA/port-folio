7efb247021fe7da5cd7aee390777987fdf62ad88
RAHEEMUDHEEN-MA <raheemudheenma118@gamil.com>
2024-04-26 00:52:06 +0530

    download btn 2

--- src/Components/DownloadBtn.jsx
+++ src/Components/DownloadBtn.jsx
@@ -9,7 +9,7 @@ const DownloadBtn = () => {
       animate={{opacity:1,}}
       transition={{ duration: 4, ease: "easeInOut" }}
 
-      className="bg-gradient-to-br from-pink-900 to-purple-800 rounded-md px-5 py-2 text-gray-300 hover:text-white hover:bg-purple-700 animate-none">
+      className="border   rounded-md px-5 py-2 text-gray-400 hover:text-white   animate-none">
             <a href="https://drive.google.com/file/d/1h_aCJ_JLGvAigLIe-ceNQPTufKFyutU_/view" target="_blank" rel="noopener noreferrer">Download CV</a>
           </motion.button>
 
